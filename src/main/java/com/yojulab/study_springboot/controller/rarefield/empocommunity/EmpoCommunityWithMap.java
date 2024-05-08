0583e50d76459efd64dff35af223cbde9b50e5b0
scvgong <gongmyeongyun@gmail.com>
2024-05-08 03:14:59 +0000

    modifiy

--- src/main/java/com/yojulab/study_springboot/controller/rarefield/empocommunity/EmpoCommunityWithMap.java
+++ src/main/java/com/yojulab/study_springboot/controller/rarefield/empocommunity/EmpoCommunityWithMap.java
@@ -15,7 +15,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.yojulab.study_springboot.service.CarInforsService;
+import com.yojulab.study_springboot.service.rarefield.empocommunity.EmpoCommunityService;
+
 
 @Controller
 @RequestMapping("/carInfor/map")
@@ -23,42 +24,42 @@ public class EmpoCommunityWithMap {
     // pagination controller 만들기, select, delete, update
 
     @Autowired
-    CarInforsService carInforsService;
+    EmpoCommunityService empoCommunityService;
 
     // /selectSearch?search=YEAR&words=2020
     // /selectSearch/CAR_NAME/소
-    @GetMapping("/selectSearch")
-    public ModelAndView selectSearch(@RequestParam Map params
-                            , ModelAndView modelAndView) {
-        // Object result = carInforsService.selectSearch(params);
-        Object result = carInforsService.selectSearchWithPagination(params);
-        modelAndView.addObject("params", params);
-        modelAndView.addObject("result", result);
+    // @GetMapping("/selectSearch")
+    // public ModelAndView selectSearch(@RequestParam Map params
+    //                         , ModelAndView modelAndView) {
+    //     // Object result = carInforsService.selectSearch(params);
+    //     Object result = empoCommunityService.selectSearchWithPagination(params);
+    //     modelAndView.addObject("params", params);
+    //     modelAndView.addObject("result", result);
         
-        modelAndView.setViewName("/WEB-INF/views/carinfor/list_map.jsp");
-        return modelAndView;
-    }
+    //     modelAndView.setViewName("/WEB-INF/views/carinfor/list_map.jsp");
+    //     return modelAndView;
+    // }
 
-    // delete with MVC
-    @PostMapping("/deleteAndSelectSearch/{UNIQUE_ID}")
-    public ModelAndView deleteAndSelectSearch(@PathVariable String UNIQUE_ID
-                        , @RequestParam Map params, ModelAndView modelAndView) {
-        Object result = carInforsService.deleteAndSelectSearch(UNIQUE_ID, params);
-        modelAndView.addObject("params", params);
-        modelAndView.addObject("result", result);
+    // // delete with MVC
+    // @PostMapping("/deleteAndSelectSearch/{UNIQUE_ID}")
+    // public ModelAndView deleteAndSelectSearch(@PathVariable String UNIQUE_ID
+    //                     , @RequestParam Map params, ModelAndView modelAndView) {
+    //     Object result = empoCommunityService.deleteAndSelectSearch(UNIQUE_ID, params);
+    //     modelAndView.addObject("params", params);
+    //     modelAndView.addObject("result", result);
 
-        modelAndView.setViewName("/WEB-INF/views/carinfor/list_map.jsp");
-        return modelAndView;
-    }
+    //     modelAndView.setViewName("/WEB-INF/views/carinfor/list_map.jsp");
+    //     return modelAndView;
+    // }
 
-    // /selectDetail/CI002
-    @GetMapping("/selectDetail/{UNIQUE_ID}")
-    public ModelAndView selectDetail(@PathVariable String UNIQUE_ID
-                        , @RequestParam Map params, ModelAndView modelAndView) {
-        Object result = carInforsService.selectDetail(UNIQUE_ID, params);
-        modelAndView.addObject("params", params);
+    // // /selectDetail/CI002
+    // @GetMapping("/selectDetail/{UNIQUE_ID}")
+    // public ModelAndView selectDetail(@PathVariable String UNIQUE_ID
+    //                     , @RequestParam Map params, ModelAndView modelAndView) {
+    //     Object result = empoCommunityService.selectDetail(UNIQUE_ID, params);
+    //     modelAndView.addObject("params", params);
 
-        modelAndView.setViewName("/WEB-INF/views/carinfor/list_map.jsp");
-        return modelAndView;
-    }
+    //     modelAndView.setViewName("/WEB-INF/views/carinfor/list_map.jsp");
+    //     return modelAndView;
+    // }
 }
--- src/main/java/com/yojulab/study_springboot/service/rarefield/empocommunity/EmpoCommunityService.java
+++ src/main/java/com/yojulab/study_springboot/service/rarefield/empocommunity/EmpoCommunityService.java
@@ -1,13 +1,18 @@
 package com.yojulab.study_springboot.service.rarefield.empocommunity;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import org.springframework.security.core.Authentication;
 import com.yojulab.study_springboot.dao.RareSharedDao;
 import com.yojulab.study_springboot.utils.Commons;
+import com.yojulab.study_springboot.utils.Paginations;
 
 @Service
 @Transactional
@@ -19,16 +24,83 @@ public class EmpoCommunityService {
     @Autowired
     Commons commons;
 
+    
+
     public Object insert(Map dataMap) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        // user_ID 가져오기
+        Object principal = authentication.getPrincipal();
+        UserDetails userDetails = (principal instanceof UserDetails) ? (UserDetails) principal : null;
+        String user_ID = userDetails.getUsername();
+        String authorities = authentication.getAuthorities().toString();
         
-        // 1. community_ID 번호를 생성 + insert 쿼리문 불러오고 대입
+        dataMap.put("user_ID",user_ID);
+
+        // community_ID 번호를 생성 + insert 쿼리문 불러오고 대입
         String sqlMapId = "RarefieldEmpocommunity.insert";
         String community_ID = commons.getUniqueSequence();
         dataMap.put("community_ID",community_ID);
-        
-        // 2. user_ID 가져오기
-        
+                
         Object result = RaresharedDao.insert(sqlMapId, dataMap);
         return result;
     }
+
+    // // 검색(조건-search : YEAR, CAR_NAME)
+    // public Map selectSearchWithPagination(Map dataMap) {
+    //     // 페이지 형성 위한 계산
+    //     int totalCount = (int) this.selectTotal(dataMap);
+        
+    //     int currentPage = 1;
+    //     if(dataMap.get("currentPage") != null) {
+    //         currentPage = Integer.parseInt((String)dataMap.get("currentPage"));    // from client in param
+    //     }
+
+    //     Paginations paginations = new Paginations(totalCount, currentPage);
+    //     HashMap result = new HashMap<>();
+    //     result.put("paginations", paginations); // 페이지에 대한 정보
+
+    //     // page record 수
+    //     String sqlMapId = "CarInfors.selectSearchWithPagination";
+    //     dataMap.put("pageScale", paginations.getPageScale());
+    //     dataMap.put("pageBegin", paginations.getPageBegin());
+        
+    //     result.put("resultList", RaresharedDao.getList(sqlMapId, dataMap)); // 표현된 레코드 정보
+    //     return result;
+    // }
+
+    // // MVC view
+    // public Object deleteAndSelectSearch(String UNIQUE_ID, Map dataMap) {
+    //     dataMap.put("", UNIQUE_ID);
+
+    //     HashMap result = new HashMap<>();
+    //     result.put("deleteCount", this.delete(dataMap));
+
+    //     result.putAll(this.selectSearchWithPagination(dataMap));
+    //     return result;
+    // }
+
+    // public Object selectDetail(String CAR_INFOR_ID, Map dataMap) {
+    //     // Object getOne(String sqlMapId, Object dataMap)
+    //     String sqlMapId = "";
+    //     dataMap.put("", CAR_INFOR_ID);
+
+    //     Object result = RaresharedDao.getOne(sqlMapId, dataMap);
+    //     return result;
+    // }
+
+    // public Object selectTotal(Map dataMap) {
+    //     String sqlMapId = "";
+
+    //     Object result = RaresharedDao.getOne(sqlMapId, dataMap);
+    //     return result;
+    // }    
+
+    // // MVC view
+    // public Object delete(Map dataMap) {
+    //     String sqlMapId = "";
+
+    //     Object result = RaresharedDao.delete(sqlMapId, dataMap);
+    //     return result;
+    // }
 }
