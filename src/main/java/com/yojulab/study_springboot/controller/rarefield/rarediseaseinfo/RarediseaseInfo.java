3853fb052da1c522e13e09c16bbce0c8550cb64d
DeokJae Kim <151099214+kdj0712@users.noreply.github.com>
2024-05-21 00:37:53 +0000

    commit

--- src/main/java/com/yojulab/study_springboot/controller/rarefield/rarediseaseinfo/RarediseaseInfo.java
+++ src/main/java/com/yojulab/study_springboot/controller/rarefield/rarediseaseinfo/RarediseaseInfo.java
@@ -5,18 +5,14 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 
-import com.google.gson.Gson;
-import com.yojulab.study_springboot.utils.PaginationUtils;
 import com.yojulab.study_springboot.utils.Paginations;
 import com.yojulab.study_springboot.service.rarefield.rest.RestTemplateService;
 
